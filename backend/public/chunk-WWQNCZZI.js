e289570e898e8217d543d17274e701301cb316a0
Vlad Epov <vladep622@gmail.com>
2024-04-21 16:02:39 +0300

    new backend build

---
+++ backend/public/chunk-WWQNCZZI.js
@@ -0,0 +1,1 @@
+import{La as r,Ma as i,X as n,ab as m,cb as a}from"./chunk-LVV5WS7J.js";var p=(()=>{let e=class e{};e.\u0275fac=function(o){return new(o||e)},e.\u0275cmp=n({type:e,selectors:[["home"]],standalone:!0,features:[a],decls:2,vars:0,template:function(o,c){o&1&&(r(0,"p"),m(1,"home works!"),i())}});let t=e;return t})();var u=[{path:"",component:p}];export{u as routes};
--- backend/public/chunk-XMABM7YD.js
+++
@@ -1,1 +0,0 @@
-import{La as r,Ma as a,X as n,ab as i,cb as p}from"./chunk-ZLUOWPJD.js";var s=(()=>{let t=class t{};t.\u0275fac=function(o){return new(o||t)},t.\u0275cmp=n({type:t,selectors:[["about"]],standalone:!0,features:[p],decls:2,vars:0,template:function(o,c){o&1&&(r(0,"p"),i(1,"about works!"),a())}});let e=t;return e})();var l=[{path:"",component:s}];export{l as routes};
