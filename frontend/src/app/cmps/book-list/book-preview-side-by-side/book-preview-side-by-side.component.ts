955ef42210b1bc74a69fc7cbd7e3b9f37ce239b9
Vlad Epov <vladep622@gmail.com>
2024-05-08 10:29:30 +0300

    added placeholder missing-image when cover is missing, and improved responsiveness in route book/genre/:id

--- frontend/src/app/cmps/book-list/book-preview-side-by-side/book-preview-side-by-side.component.ts
+++ frontend/src/app/cmps/book-list/book-preview-side-by-side/book-preview-side-by-side.component.ts
@@ -1,9 +1,10 @@
+import { NgIf } from '@angular/common'
 import { Component, Input } from '@angular/core'
 
 @Component({
   selector: 'book-preview-side-by-side',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './book-preview-side-by-side.component.html',
   styleUrl: './book-preview-side-by-side.component.scss'
 })
--- frontend/src/app/cmps/book-list/book-preview-top-to-bottom/book-preview-top-to-bottom.component.ts
+++ frontend/src/app/cmps/book-list/book-preview-top-to-bottom/book-preview-top-to-bottom.component.ts
@@ -1,9 +1,10 @@
+import { NgIf } from '@angular/common'
 import { Component, Input } from '@angular/core'
 
 @Component({
   selector: 'book-preview-top-to-bottom',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './book-preview-top-to-bottom.component.html',
   styleUrl: './book-preview-top-to-bottom.component.scss'
 })
