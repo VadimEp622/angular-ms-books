98086cfd11282035d0e2c33c4ed5b400cbf045f7
Vlad Epov <vladep622@gmail.com>
2024-05-12 09:18:05 +0300

    updated todos

--- backend/middlewares/cache.middleware.mjs
+++ backend/middlewares/cache.middleware.mjs
@@ -1,7 +1,8 @@
 import { bookCache } from "../services/cache.service.mjs"
 import { logger } from "../services/logger.service.mjs"
 
-
+// TODO: consider doing caching by url, so /api/genre will be the cache key. consider type of request as well (GET, POST, etc). (no reason currently to use others beside GET)
+//   This will allow having only one cache function, which we can reuse for diferent routes.
 
 export function cacheBooksByGenres(req, res, next) {
     try {
