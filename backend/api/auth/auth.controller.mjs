7c59bfac3948db54e6884517a648d9a7a2544f13
Vlad Epov <vladep622@gmail.com>
2024-05-14 14:55:41 +0300

    removed redundant and fault code regarding als, and updated notes

--- backend/api/auth/auth.controller.mjs
+++ backend/api/auth/auth.controller.mjs
@@ -41,10 +41,7 @@ export async function signup(req, res) {
 
 export async function logout(req, res) {
     try {
-        // TODO: check if deleting loggedinUser like this is in line with asyncLocalStorage best practices
         logger.info('user logout')
-        const store = asyncLocalStorage.getStore()
-        delete store.loggedinUser
         res.clearCookie('loginToken')
         res.send({ msg: 'Logged out successfully' })
     } catch (err) {
--- backend/middlewares/setupAls.middleware.mjs
+++ backend/middlewares/setupAls.middleware.mjs
@@ -1,6 +1,11 @@
 import { authService } from '../api/auth/auth.service.mjs'
 import { asyncLocalStorage } from '../services/als.service.mjs'
 
+
+// INFO: when user performs logout, the store.loggedinUser is set to undefined.
+//     that is because logout clears the front-end's cookie.
+//     if the front-end's cookie is not cleared, the store's loggedinUser will persist. (or is it that each server call, will cause the function to run again?)
+//     research to make absolutely sure.
 export async function setupAsyncLocalStorage(req, res, next) {
   const storage = {}
   asyncLocalStorage.run(storage, () => {
