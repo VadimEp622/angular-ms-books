8ba8bed97217cb3e4883fde49d9ca8ef0edede21
Vlad Epov <vladep622@gmail.com>
2024-06-18 22:45:55 +0300

    added logo cmp to header desktop

--- frontend/src/app/cmps/app-header/header-desktop-main/header-desktop-main.component.ts
+++ frontend/src/app/cmps/app-header/header-desktop-main/header-desktop-main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ProfileActionMenuComponent } from './profile-action-menu/profile-action-menu.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { LogoComponent } from '../../logo/logo.component';
 
 @Component({
   selector: 'header-desktop-main',
   standalone: true,
-  imports: [ProfileActionMenuComponent, SearchBarComponent],
+  imports: [ProfileActionMenuComponent, SearchBarComponent, LogoComponent],
   templateUrl: './header-desktop-main.component.html',
   styleUrl: './header-desktop-main.component.scss',
 })
@@ -15,8 +16,6 @@ export class HeaderDesktopMainComponent {
   // - search bar
   // - user profile
   // - (?) notification
-
-
   // TODO: searchbar ->
   //                    1) make a route for search results
   //                    2) make API for querying search results
