01f6eca1929b697f650f896f6508e292b6ab71bc
Vlad Epov <vladep622@gmail.com>
2024-06-19 05:01:40 +0300

    added template user route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,30 +1,41 @@
-import { Routes } from '@angular/router'
-import { DynamicLayoutComponent } from './layout/dynamic-layout/dynamic-layout.component'
+import { Routes } from '@angular/router';
+import { DynamicLayoutComponent } from './layout/dynamic-layout/dynamic-layout.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        title: 'home',
-        pathMatch: 'full',
-        component: DynamicLayoutComponent,
-        loadChildren: () => import('./pages/home/home.routes').then(m => m.routes)
-    },
-    {
-        path: 'book',
-        title: 'book',
-        component: DynamicLayoutComponent,
-        loadChildren: () => import('./pages/book/book.routes').then(m => m.routes)
-    },
-    {
-        path: 'about',
-        title: 'about',
-        component: DynamicLayoutComponent,
-        loadChildren: () => import('./pages/about/about.routes').then(m => m.routes)
-    },
-    {
-        path: '**',
-        title: 'error',
-        component: DynamicLayoutComponent,
-        loadChildren: () => import('./pages/error/error.routes').then(m => m.routes)
-    }
-]
+  {
+    path: '',
+    title: 'home',
+    pathMatch: 'full',
+    component: DynamicLayoutComponent,
+    loadChildren: () =>
+      import('./pages/home/home.routes').then((m) => m.routes),
+  },
+  {
+    path: 'book',
+    title: 'book',
+    component: DynamicLayoutComponent,
+    loadChildren: () =>
+      import('./pages/book/book.routes').then((m) => m.routes),
+  },
+  {
+    path: 'user',
+    title: 'user',
+    component: DynamicLayoutComponent,
+    loadChildren: () =>
+      import('./pages/user/user.routes').then((m) => m.routes),
+  },
+  {
+    path: 'about',
+    title: 'about',
+    component: DynamicLayoutComponent,
+    loadChildren: () =>
+      import('./pages/about/about.routes').then((m) => m.routes),
+  },
+  {
+    path: '**',
+    title: 'error',
+    component: DynamicLayoutComponent,
+    loadChildren: () =>
+      import('./pages/error/error.routes').then((m) => m.routes),
+  },
+];
