707f200afb48460fa6237e3043848b4f475e6807
Vlad Epov <vladep622@gmail.com>
2024-06-20 07:54:00 +0300

    1. updated static front-end 2. updated notes

--- frontend/src/app/pages/user/user.component.ts
+++ frontend/src/app/pages/user/user.component.ts
@@ -9,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class UserComponent {
 
+  // TODO: do resolver which awaits backend user data from cookie token, and then renders user page.
+
+  // TODO: mobile footer user icon -> (?) when clicked, 
+  //      IF not logged in (no cookie token), open grayed-background login modal.
+  //      IF logged in (has cookie token), route to user page.
+
 }
