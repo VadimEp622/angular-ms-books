ab8be17a9b4dada7173c9b0edb775674c5d1941c
Vlad Epov <vladep622@gmail.com>
2024-07-06 13:40:22 +0300

    updated TODOS

--- frontend/src/app/pages/book/book-details/book-details.component.ts
+++ frontend/src/app/pages/book/book-details/book-details.component.ts
@@ -21,9 +21,7 @@ export class BookDetailsComponent implements OnInit, OnDestroy {
 
   // TODO:
   //    1) Make page responsive
-  //    2) make layout centered (with wider left-right "padding-like")
-  //    3) add authors from backend
-  //    4) add missing cover icon/image
+  //    2) add missing cover icon/image
 
   ngOnInit() {
     this.sub = this.route.data
